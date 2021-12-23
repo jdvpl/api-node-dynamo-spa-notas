1d4af15b7eba5ce33d8f2b9a3b15bf70ae05d384
JDVPL <juanda554242@gmail.com>
2021-12-23 12:48:54 -0500

    backend ok

--- app/routes/app-routes.js
+++ app/routes/app-routes.js
@@ -9,19 +9,17 @@ const AWS=require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' })
 
 const docClient=new AWS.DynamoDB.DocumentClient(); 
-const tableName='prueba_table1';
+const tableName='td_notes_test';
 // ejemplo
-var user_id='1';
-var user_name=' Usuario nuevo';
 
+let user_id='1'
 // endpoint para crear nota
 router.post('/crear/nota', (req, res, next)=>{
 
     let item=req.body.Item;
     item.user_id=user_id;
-    item.user_name=user_name;
-    item.note_id=user_id +':'+ uuidv4();
-    item.time=moment().unix();
+    item.note_id= uuidv4();
+    item.timestamp=moment().unix();
     item.expires=moment().add(90, 'days').unix();
 
     // agrega a la base de datos
@@ -42,11 +40,10 @@ router.post('/crear/nota', (req, res, next)=>{
     })
 });
 // endpoint para actualizar nota
-router.patch('/actualizar/nota', (req, res, next)=>{
-
+router.patch('/actualizar/nota/:user_id', (req, res, next)=>{
+    let user_id=req.params.user_id;
     let item=req.body.Item;
     item.user_id=user_id;
-    item.user_name=user_name;
 
     item.expires=moment().add(90, 'days').unix();
 
@@ -56,10 +53,10 @@ router.patch('/actualizar/nota', (req, res, next)=>{
         Item:item,
         ConditionExpression: '#t = :t',
         ExpressionAttributeNames: {
-            '#t':'time'
+            '#t':'timestamp'
         },
         ExpressionAttributeValues: {
-            ':t':item.time
+            ':t':item.timestamp
         }
     },(err, data)=>{
         if(err){
@@ -76,5 +73,97 @@ router.patch('/actualizar/nota', (req, res, next)=>{
     })
 });
 
+// metodo get
+router.get('/notas', (req, res, next)=>{
+    // limite de notas
+    let limit=req.query.limiit ? parseInt(req.query.limiit) : 5;
+    let params={
+        TableName:tableName,
+        KeyConditionExpression: "user_id = :uid",
+        ExpressionAttributeValues: {
+            ":uid":user_id,
+        },
+        Limit:limit,
+        ScanIndexForward: false
+    };
+    // starttimestamp es para colocar en la url parte de la pagina cion
+    let starttimestamp=req.query.start ? parseInt(req.query.start) : 0;
+    if(starttimestamp>0){
+        params.ExclusiveStartKey={
+            user_id:user_id,
+            timestamp: starttimestamp
+        }
+    }
+    // base de datos
+    docClient.query(params, (err, data)=>{
+        if(err){
+            console.log(err);
+            return res.status(err.statusCode).send({
+                message: err.message,
+                status: err.statusCode
+            })
+        }else{
+            return res.status(200).send(data)
+        }
+    })
+})
 
+// obtener nota por la nota_id
+router.get('/nota/:timestamp/:user_id', (req, res, next)=>{
+    let timestamp=parseInt(req.params.timestamp);
+    let user_id=req.params.user_id;
+    let params={
+        TableName:tableName,
+        IndexName:'note_id-index',
+        ExpressionAttributeNames: {
+            '#timestamp':'timestamp',
+            '#user_id':'user_id',
+        },
+        KeyConditionExpression: "#timestamp = :timestamp and #user_id = :user_id",
+        ExpressionAttributeValues: {
+            ":user_id":user_id,
+            ":timestamp":timestamp,
+        },
+        Limit:1
+    };
+
+    docClient.query(params, (err, data)=>{
+        if(err){
+            console.log(err);
+            return res.status(err.statusCode).send({
+                message:err.message,
+                status: err.statusCode
+            })
+        }else{
+            if(!_.isEmpty(data.Items)){
+                return res.status(200).send(data.Items[0])
+            }else{
+                return res.status(400).send()
+            }
+        }
+    })
+})
+
+router.delete('/borrarnota/:timestamp/:user_id',(req,res,next)=>{
+    let timestamp=parseInt(req.params.timestamp);
+    let user_id=req.params.user_id;
+    let params={
+        TableName: tableName,
+        Key:{
+            user_id:user_id,
+            timestamp: timestamp
+        }
+    };
+    docClient.delete(params, (err,data)=>{
+        if(err){
+            console.log(err);
+            return res.status(err.statusCode).send({
+                message: err.message,
+                status: err.statusCode
+            })
+        }else{
+            return res.status(200).send()
+        }
+    })
+})
 module.exports=router;
